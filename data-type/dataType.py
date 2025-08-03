872035a67ece70ff1c547db088f66901cb690440
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-03 23:13:19 +0600

    study on datatype

---
+++ data-type/dataType.py
@@ -0,0 +1,37 @@
+# Python has the following data types built-in by default, in these categories:
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+x = "Hello world!"
+print(x)
+print(type(x))
+
+# list 
+x = ["apple", "bananna", "orange"]
+print(x)
+print(type(x))
+
+# tuple
+x = ("apple", "bananna", "orange")
+print(x)
+print(type(x))
+
+# dict
+x = {
+    "name": "SOHEL",
+    "age": 26
+}
+print(x)
+print(type(x))
+
+# set
+x = {"apple", "orange", "bananna"}
+print(x)
+print(type(x))
