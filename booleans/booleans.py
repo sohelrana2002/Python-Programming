c3bd6c07698039157dbb77754e7c3186e41733ed
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-04 23:58:58 +0600

    study on boolean

---
+++ booleans/booleans.py
@@ -0,0 +1,26 @@
+print(10 >= 9) # output True
+print(20 == 30) # output False
+print(30 < 10) # output False
+
+a = 30
+b = 20
+
+if a > b:
+    print("a is greater then b")
+else:
+    print("b is greater then a")
+
+print(bool("Hello")) # output True
+print(bool(15)) # output True
+
+print(bool("abc")) # output True
+print(bool(123)) # output True
+print(bool(["apple", "cherry", "banana"])) # output True
+
+print(bool(False)) # output False
+print(bool(None)) # output False
+print(bool(0)) # output False
+print(bool("")) # output False
+print(bool(())) # output False
+print(bool([])) # output False
+print(bool({})) # output False
