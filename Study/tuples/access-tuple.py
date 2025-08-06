94c868bed71133a8080a5e2bf45a88fc409f83b6
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-06 23:45:51 +0600

    study on access tuple

---
+++ Study/tuples/access-tuple.py
@@ -0,0 +1,6 @@
+# Access Tuple Items
+# You can access tuple items by referring to the index number, inside square brackets:
+thistuple = ("apple", "bananna", "orange")
+print(thistuple[1]) # bananna
+
+# tuple accessing is same as the list
