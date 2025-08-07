d80fb6e7b2a5505ada8a28424dbedc58350a7e0a
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-07 23:19:39 +0600

    study on add set item

---
+++ Study/set/add-set-item.py
@@ -0,0 +1,26 @@
+# Add Items
+# Once a set is created, you cannot change its items, but you can add new items.
+
+# To add one item to a set use the add() method.
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset) # {'banana', 'apple', 'cherry', 'orange'}
+
+# To add items from another set into the current set, use the update() method.
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset) # {'papaya', 'cherry', 'mango', 'pineapple', 'apple', 'banana'}
+
+# Add Any Iterable
+# The object in the update() method does not have to be a set, 
+# it can be any iterable object (tuples, lists, dictionaries etc.).
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset) # {'banana', 'orange', 'apple', 'kiwi', 'cherry'}
