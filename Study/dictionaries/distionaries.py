6f938ca06a596fb4bdc731226b266b50381c42a2
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-08 23:53:07 +0600

    study on dictionaries

---
+++ Study/dictionaries/distionaries.py
@@ -0,0 +1,45 @@
+# Dictionary
+# Dictionaries are used to store data values in key:value pairs.
+
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+# Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"]) # Ford
+
+# Dictionary Length
+# To determine how many items a dictionary has, use the len() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(len(thisdict)) # 3
+
+# Dictionary Items - Data Types
+# The values in dictionary items can be of any data type:
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict)
+
+# The dict() Constructor
+# It is also possible to use the dict() constructor to make a dictionary.
+thisdict = dict(name = "John", age = 36, country = "Norway")
+
+print(thisdict) 
