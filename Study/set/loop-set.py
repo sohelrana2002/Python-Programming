6d9425ea27b95b494b2c4297d31a688e69a24c1d
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-08 23:11:56 +0600

    study on loop set

---
+++ Study/set/loop-set.py
@@ -0,0 +1,20 @@
+# You can loop through the set items by using a for loop:
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+# important you can't loop like that
+# thisset = {"apple", "banana", "cherry"}
+
+# for i in range(len(thisset)):
+#   print(thistuple[i])
+
+# important you can't loop like that
+# thisset = {"apple", "banana", "cherry"}
+# i = 0
+
+# while i < len(thisset):
+#     print(thisset[i])
+#     i = i + 1
