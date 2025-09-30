1040e6232ed9ca75dec2164934845f3c3eadf47c
SOHEL RANA <sohelrana104322@gmail.com>
2025-09-30 10:37:17 +0600

    study on decorator

---
+++ Study/decorator/decorator.py
@@ -0,0 +1,26 @@
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Hello Sally"
+
+@changecase
+def otherfunction():
+  return "I am speed!"
+
+print(myfunction())
+print(otherfunction())
+
+def changecase(func):
+  def myinner(x):
+    return func(x).upper()
+  return myinner
+
+@changecase
+def myfunction(nam):
+  return "Hello " + nam
+
+print(myfunction("John"))
